2ddf224f444ddfd82305942d1f57018cc915a8b5
Wendy <wen.mcnair@gmail.com>
2024-03-08 20:21:59 -0500

    replaced getAll function with a fetch statement

--- src/restdb.js
+++ src/restdb.js
@@ -19,10 +19,36 @@ const items = [
     }
   ]
 
-// GET all customers
-export function getAll(){
-    return items;
+//// GET all customers
+// export function getAll(){
+//     return items;
+// }
+
+const baseURL = 'http://localhost:4000/customers';
+
+export async function getAll(setCustomers) {
+  const myInit = {
+      method: 'GET',
+      mode: 'cors' };
+  const fetchData = async (url) => {
+      try {
+              const response = await fetch(url, myInit);
+              if (!response.ok) {
+                throw new Error(`Error fetching data: ${response.status}`);
+              }
+              const data = await response.json();
+              setCustomers(data);
+          } catch (error) {
+              alert(error);
+          } 
+  }
+
+  fetchData(baseURL);
 }
+
+
+
+
 
 export function get(id) {
     let result = null;
