dbdc13d192fad1d14008d7d6843116cdcf158443
Wendy <wen.mcnair@gmail.com>
2024-03-08 21:30:19 -0500

    removed obsolete code

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ export function App(params) {
       
 
 
-  {/* 
+  
       <div> <AddUpdateForm
                 mode={mode}
                 handleInputChange={handleInputChange}
@@ -95,60 +95,13 @@ export function App(params) {
                 onCancelClick={onCancelClick}
             />
       </div>
-  */}
+  
 
 
 
 
 
-      {/*  
-      <div className="boxed">
-        <div>
-          <h4>{mode}</h4>
-        </div>
-        <form >
-          <table id="customer-add-update" >
-            <tbody>
-              <tr>
-                <td className={'label'} >Name:</td>
-                <td><input
-                  type="text"
-                  name="name"
-                  onChange={(e) => handleInputChange(e)}
-                  value={formObject.name}
-                  placeholder="Customer Name"
-                  required /></td>
-              </tr>
-              <tr>
-                <td className={'label'} >Email:</td>
-                <td><input
-                  type="email"
-                  name="email"
-                  onChange={(e) => handleInputChange(e)}
-                  value={formObject.email}
-                  placeholder="name@company.com" /></td>
-              </tr>
-              <tr>
-                <td className={'label'} >Pass:</td>
-                <td><input
-                  type="text"
-                  name="password"
-                  onChange={(e) => handleInputChange(e)}
-                  value={formObject.password}
-                  placeholder="password" /></td>
-              </tr>
-              <tr className="button-bar">
-                <td colSpan="2">
-                  <input type="button" value="Delete" onClick={onDeleteClick} />
-                  <input type="button" value="Save" onClick={onSaveClick} />
-                  <input type="button" value="Cancel" onClick={onCancelClick} />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </div>
-       */}
+      
 
     </div>
   );
